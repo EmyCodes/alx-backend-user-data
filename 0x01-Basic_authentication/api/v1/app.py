62ea7cf3df211214ec756df67b6e42521f8c11fe
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-08-11 01:05:43 +0100

    Revert "feat: added unauthorised route"
    This reverts commit 43441cbdfa380cd6a334a51bd4c36a20ac95db86.

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -18,15 +18,7 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 def not_found(error) -> str:
     """ Not found handler
     """
-    # return jsonify({f"{error}": "Not found"}), 404
-    return jsonify({"error": "Not found"}), 404
-
-
-@app.unauthorized(401)
-def not_authorized(error="error") -> str:
-    """ Unauthorized
-    """
-    return jsonify({"error": "Unauthorized"}), 401
+    return jsonify({"error": "Unauthorized"}), 404
 
 
 if __name__ == "__main__":
